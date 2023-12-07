1329e3b66019c4a6ad73de4095f4d28050d44ed4
nidzammst <akamhacks@gmail.com>
2023-12-08 06:37:44 +0700

    ignoring build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true,
   },
   experimental: {
     serverActions: true,
